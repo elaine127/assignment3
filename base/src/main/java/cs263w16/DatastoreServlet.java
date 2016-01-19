3405dc4bc240b3db39c68379750b1bf2ae90b6dd
elaineyang <elaineyang127@gmail.com>
2016-01-18 19:23:31 -0800

    memcache step 4

--- base/src/main/java/cs263w16/DatastoreServlet.java
+++ base/src/main/java/cs263w16/DatastoreServlet.java
@@ -19,6 +19,8 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 //import com.google.appengine.repackaged.com.google.datastore.v1.Filter;
 
 @SuppressWarnings("serial")
@@ -42,6 +44,7 @@ public class DatastoreServlet extends HttpServlet {
     	  }
     	
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
       String keyname = req.getParameter("keyname");
       String value = req.getParameter("value");
       if (keyname != null) {
@@ -53,32 +56,54 @@ public class DatastoreServlet extends HttpServlet {
       		tne.setProperty("date", date);
       		datastore.put(tne);
       		resp.getWriter().println("<h3>Stored KEY: "+ keyname+ " and VALUE: "+ value+ " in Datastore</h3>");
+      		
+//      	syncCache.put(keyname, value);
+//    		resp.getWriter().println("<h3>Stored KEY: "+ keyname+ " and VALUE: "+ value+ " in Memcache</h3>");  	     
+//      	
       	}
       	else{
       		//Display the element of kind TaskData in the
-      		    Key k = KeyFactory.createKey("TaskData", keyname);
-      		    Filter keyFilter =new FilterPredicate(Entity.KEY_RESERVED_PROPERTY,FilterOperator.EQUAL,k);
-      			Query findValue = new Query().setFilter(keyFilter);
-      			List<Entity> result = datastore.prepare(findValue).asList(FetchOptions.Builder.withDefaults());
-      			resp.getWriter().println("<h3>Display after filtering</h3> ");
-      			for(int i =0; i < result.size(); i++){
-        		  Entity e = result.get(i);
-        		  resp.getWriter().println("<h3>Stored KEY: "+ keyname +  " and VALUE: "+ e.getProperty("value")+ " in Datastore</h3>");
-      			}
-//					Entity tne = datastore.get(k);
-//      		    value = (String) tne.getProperty("value");
-//      			Date date  = (Date) tne.getProperty("date");
-//      			resp.getWriter().println("<h3>Hello World"+ date +":"+value+ "</h3>");
-//      	
-      	}
+      		    //check memcache fist
+      		    if(!syncCache.contains(keyname))
+      		    {
+      		    	resp.getWriter().println("Not in MemCache!");
+      		    
+      		    //check ds
+      		    	Key k = KeyFactory.createKey("TaskData", keyname);
+      		    	Filter keyFilter =new FilterPredicate(Entity.KEY_RESERVED_PROPERTY,FilterOperator.EQUAL,k);
+      		    	Query findValue = new Query().setFilter(keyFilter);
+      		    	List<Entity> result = datastore.prepare(findValue).asList(FetchOptions.Builder.withDefaults());
+      		    	if(result.size()==0) 
+      		    		resp.getWriter().println("Not in DataStore!");
+      		    	else{
+      		    		resp.getWriter().println("In DataStore!");
+      		    		// copy one to the memcache	
+      		    		for(int i =0; i < result.size(); i++){
+      		    			Entity e = result.get(i);
+      		    			syncCache.put(keyname, e.getProperty("value"));
+      		    			resp.getWriter().println("<h3>Stored KEY: "+ keyname +  " and VALUE: "+ e.getProperty("value")+ " in Datastore</h3>");
+      		    			}      	
+      		    		}
+      		    }else
+      		    	{
+      		    	resp.getWriter().println("In Memcache!");
+      		    	resp.getWriter().println("<h3>KEY: "+ keyname +  " and VALUE: "+syncCache.get(keyname)+ " value"+ " in Memcache</h3>");
+    			
+      		    	}
       }
+      	}
       else{
+    	  //display the entities in datastore and memcache.
     	  Query findAll = new Query("TaskData");
     	  List<Entity> result = datastore.prepare(findAll).asList(FetchOptions.Builder.withDefaults());
-    	  resp.getWriter().println("<h3>Display existing</h3> ");
     	  for(int i =0; i < result.size(); i++){
     		  Entity e = result.get(i);
-    		  resp.getWriter().println("<h3> Created Date: "+ e.getProperty("date") +  " and VALUE: "+ e.getProperty("value")+ " in Datastore</h3>");
+    		  
+    		  resp.getWriter().println("<h3> DataStore Created Date: "+ e.getProperty("date") +  " and VALUE: "+ e.getProperty("value")+"</h3>");
+    		  if(syncCache.contains(e.getKey().getName()))
+    			  {
+    			  resp.getWriter().println("<h3> Memcache Created Date: "+ e.getProperty("date") +  " and VALUE: "+ e.getProperty("value")+ "</h3>");
+    			  }
   			}
       }
 
